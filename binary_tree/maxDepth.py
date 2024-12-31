698b1d3d88d1f253bcb2c9cfe0c33b36887d51a1
ning.zhou <zztaurus@163.com>
2024-12-31 18:12:35 +0800

    update

---
+++ binary_tree/maxDepth.py
@@ -0,0 +1,26 @@
+
+# 104, 二叉树的最大深度
+# 树的遍历: 深度优先(DFS)遍历和广度优先(BFS)遍历
+
+
+def maxDepth(self, root):
+    if root is None:
+        return 0
+    else:
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+def maxDepeth(root):
+
+    if root is None:
+        return 0
+
+    queue, depth = [root], 1
+    while queue:
+
+        for node in queue:
+            queue.append(node.left)
+            queue.append(node.right)
+        depth += 1
+
+    return depth
