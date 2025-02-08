4dfdbec93bc7d1257bdc44523bef57b145396457
ning.zhou <zztaurus@163.com>
2025-02-08 18:42:05 +0800

    update

---
+++ binary_tree/midlle/recoverTree.py
@@ -0,0 +1,19 @@
+
+# 99. 恢复二叉搜索树
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def recoverTree(self, root):
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+
+
+
