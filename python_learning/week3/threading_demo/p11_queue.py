66a7998b78c83c2e2c41a2f03a67370b3af6058b
zztaurus <zztaurus@163.com>
2025-05-08 19:43:09 +0800

    update

--- python_learning/week3/threading_demo/p11_queue.py
+++ python_learning/week3/threading_demo/p11_queue.py
@@ -1,89 +1,87 @@
 import queue
-q = queue.Queue(5)
-q.put(111)        # 存队列
-q.put(222)
-q.put(333)
- 
-print(q.get())    # 取队列
-print(q.get())
-q.task_done()     # 每次从queue中get一个数据之后，当处理好相关问题，最后调用该方法，
-                  # 以提示q.join()是否停止阻塞，让线程向前执行或者退出
-print(q.qsize())  # 队列中元素的个数， 队列的大小
-print(q.empty())  # 队列是否为空
-print(q.full())   # 队列是否满了
-
-###############
-
-import queue
 import threading
 import random
 import time
 
-writelock = threading.Lock()
-
 class Producer(threading.Thread):
     def __init__(self, q, con, name):
-        super(Producer, self).__init__()
+        super().__init__()
         self.q = q
+        self.con = con
         self.name = name
-        self.con =con
+        self.running = True  # 线程退出标志
         print(f'Producer {self.name} Started')
-    
+
     def run(self):
-        while 1:
-            global writelock
-            self.con.acquire()  # 获得锁对象
+        while self.running:
+            value = random.randint(0, 10)
+            with self.con:
+                # 使用 while 防止虚假唤醒
+                while self.q.full():
+                    print(f'Producer {self.name}: Queue is full, waiting...')
+                    self.con.wait()  # 释放锁，等待唤醒
+                # 队列未满，放入数据
+                self.q.put(f'{self.name}: {value}')
+                print(f'Producer {self.name} produced {value}')
+                self.con.notify_all()  # 唤醒所有等待的线程
+            time.sleep(1)
+        print(f'Producer {self.name} exiting')
 
-            if self.q.full():   # 队列满
-                with writelock:
-                    print('Queue is full , producer wait')
-                self.con.wait()  # 等待资源
-            
-            else:
-                value = random.randint(0,10)
-                with  writelock:
-                    print(f'{self.name} put value {self.name} {str(value)} in queue')
-                self.q.put( (f'{self.name} : {str(value)}') ) # 放入队列
-                self.con.notify()   # 通知消费者
-                time.sleep(1)
-        self.con.release()
-
+    def stop(self):
+        self.running = False
 
 class Consumer(threading.Thread):
     def __init__(self, q, con, name):
-        super(Consumer, self).__init__()
+        super().__init__()
         self.q = q
+        self.con = con
         self.name = name
-        self.con =con
+        self.running = True
         print(f'Consumer {self.name} Started')
 
     def run(self):
-        while 1:
-            global writelock
-            self.con.acquire()
-            if self.q.empty():   # 队列满
-                with writelock:
-                    print('Queue is full , producer wait')
-                self.con.wait()  # 等待资源
-            else:
+        while self.running:
+            with self.con:
+                # 使用 while 防止虚假唤醒
+                while self.q.empty():
+                    print(f'Consumer {self.name}: Queue is empty, waiting...')
+                    self.con.wait()
+                # 队列非空，取出数据
                 value = self.q.get()
-                with writelock:
-                    print(f'{self.name} get value {value} from queue')              
-                self.con.notify()   # 通知生产者
-                time.sleep(1)
-        self.con.release()
+                print(f'Consumer {self.name} consumed {value}')
+                self.con.notify_all()
+                self.q.task_done()  # 标记任务完成
+            time.sleep(1)
+        print(f'Consumer {self.name} exiting')
 
-
+    def stop(self):
+        self.running = False
 
 if __name__ == '__main__':
-    q = queue.Queue(10)
-    con = threading.Condition()   # 条件变量锁
+    q = queue.Queue(maxsize=5)  # 设置队列最大容量
+    con = threading.Condition()
 
     p1 = Producer(q, con, 'P1')
-    p1.start()
     p2 = Producer(q, con, 'P2')
-    p2.start()
     c1 = Consumer(q, con, 'C1')
-    c1.start()
+    c2 = Consumer(q, con, 'C2')  # 添加更多消费者示例
 
-# 练习使用数组实现队列
+    p1.start()
+    p2.start()
+    c1.start()
+    c2.start()
+
+    # 运行一段时间后停止线程
+    time.sleep(5)
+    
+    p1.stop()
+    p2.stop()
+    c1.stop()
+    c2.stop()
+
+    p1.join()
+    p2.join()
+    c1.join()
+    c2.join()
+
+    print("All threads exited.")
