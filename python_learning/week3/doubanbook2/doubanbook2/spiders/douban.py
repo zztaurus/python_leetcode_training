b21950c0110426f159d166c5c4356e9f6b8dc1e6
周宁 <zhouning@zhouningdeMac-mini.local>
2025-05-08 16:46:23 +0800

    update

---
+++ python_learning/week3/doubanbook2/doubanbook2/spiders/douban.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DoubanSpider(scrapy.Spider):
+    name = "douban"
+    allowed_domains = ["book.douban.com"]
+    start_urls = ["https://book.douban.com"]
+
+    def parse(self, response):
+        pass
